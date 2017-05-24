1c82e6a6eb0f4dd9e3bd743adfa5b215c5a9da4f
mgarcia <mgarcia4703@aol.com>
2017-05-24 01:42:04 -0700

    updated with results

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## function provides inverse be first setting the matrix then using cache solve to return the inverse
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## this function creates the matrix for which we will compute the inverse
 
+makeCacheMatrix <- function(x = matrix() )
+{#configure the matrix x,y
+  {myinverse = NULL  #default value for inverse
+  b = function(y)
+  {x<<-y    # see scoping notes
+  inverse<<- NULL
+  }
+  }
+  a <- function()x 
+  b = function(inverse) invr <<- inverse
+  c <- function() invr
+  invlist(set=set, a=a, b=b, c=c)
 }
 
 
+
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+invResult <- function(x,...) 
+{
+  invr <- x$c() # scoping assignment used line 41
+  
+  mxData <- x$a() # assign data set
+  invr <- solve(mxData)  # solve invr scope for mxData
+  x$b(invr)
+  invr
 }
